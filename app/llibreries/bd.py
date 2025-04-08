437c862f93d02b2d03b29f39bcbf0b81cafd31ca
IevgenSoloviov <evgensoloviov12@gmail.com>
2025-04-08 15:21:50 +0200

    Update bd.py

--- app/llibreries/bd.py
+++ app/llibreries/bd.py
@@ -1,3 +1,10 @@
+"""
+Fitxer: bd.py
+
+Conté les funcions per establir connexió amb la base de dades PostgreSQL.
+Inclou mètodes per executar consultes, inserir dades i gestionar errors de connexió.
+"""
+
 # app/llibreries/bd.py
 
 import sqlite3

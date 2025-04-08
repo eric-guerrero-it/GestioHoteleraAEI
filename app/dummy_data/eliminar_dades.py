4b93c9b24bb4520931684bd152cb37af8daa4349
IevgenSoloviov <evgensoloviov12@gmail.com>
2025-04-08 15:17:16 +0200

    Que es? eliminar_dades.py

---
+++ app/dummy_data/eliminar_dades.py
@@ -0,0 +1,8 @@
+"""
+Fitxer: eliminar_dades.py
+
+Aquest fitxer s'encarrega d'eliminar les dades simulades generades per a proves 
+(de la base de dades o fitxers temporals), deixant l’entorn net per a nous tests.
+
+És útil per repetir proves múltiples vegades o fer demostracions amb base de dades buida.
+"""

a14d6041bccf645fc0217a0a5bd20fbbe438db23
IevgenSoloviov <evgensoloviov12@gmail.com>
2025-04-08 15:18:33 +0200

    que es? api_mossos.py

---
+++ app/extract/api_mossos.py
@@ -0,0 +1,8 @@
+"""
+Fitxer: api_mossos.py
+
+Aquest fitxer conté la lògica per connectar-se amb l’API externa dels Mossos d’Esquadra.
+Envia informació de les reserves de l’hotel que, per normativa, s’han de comunicar automàticament.
+
+Inclou la preparació de les dades, autenticació i enviament via peticions HTTP.
+"""

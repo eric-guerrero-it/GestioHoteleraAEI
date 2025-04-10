a61cd6679df7de3fcf03e2f80c879c32fe3980bc
Marqueskis <163524103+Marquesmarki@users.noreply.github.com>
2025-04-10 17:35:50 +0200

    Nova conexio a la base de dades

--- app/llibreries/bd.py
+++ app/llibreries/bd.py
@@ -5,9 +5,33 @@ Conté les funcions per establir connexió amb la base de dades PostgreSQL.
 Inclou mètodes per executar consultes, inserir dades i gestionar errors de connexió.
 """
 
-# app/llibreries/bd.py
-import sqlite3
+"""
+Fitxer: bd.py
 
+Conté la funció per connectar-se a la base de dades PostgreSQL.
+Llegeix les credencials des d’un fitxer 'hotel.enu' (afegit al .gitignore).
+"""
+
+import psycopg2
+
+# ───────────────────────────────────────────────
+# Connecta a la base de dades PostgreSQL utilitzant les credencials del fitxer
 def connectar_bd():
-    conn = sqlite3.connect('base_dades.db')
+    # Llegim les 4 línies del fitxer amb BD, USER, PW, IP
+    with open("hotel.enu", "r") as f:
+        linies = f.read().splitlines()
+
+    # Assignem cada línia a la variable corresponent
+    bd = linies[0].split("=")[1].strip()
+    user = linies[1].split("=")[1].strip()
+    pw = linies[2].split("=")[1].strip()
+    ip = linies[3].split("=")[1].strip()
+
+    # Connectem a PostgreSQL
+    conn = psycopg2.connect(
+        dbname=bd,
+        user=user,
+        password=pw,
+        host=ip
+    )
     return conn

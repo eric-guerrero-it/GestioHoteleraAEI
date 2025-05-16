bac10d59340899f07034054339141fab2cbfab08
IevgenSoloviov <evgensoloviov12@gmail.com>
2025-05-17 00:09:54 +0200

    Delete app/extract/powerbi.py

--- app/extract/powerbi.py
+++
@@ -1,8 +0,0 @@
-"""
-Fitxer: powerbi.py
-
-Aquest fitxer prepara i transforma dades per poder ser utilitzades en quadres de comandament de Power BI.
-Inclou exportació de dades a formats compatibles i estructures analítiques.
-
-Facilita l’anàlisi i visualització de dades del sistema hoteler per part dels responsables.
-"""
